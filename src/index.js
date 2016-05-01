3a9af0e0b4ddae2a176bc79fdd6165edcefd669f
goldsun27 <todayhonest27@gmail.com>
2016-04-30 19:52:26 -0500

    Setup Redux Form for Sign In

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 
 import App from './components/app';
 import reducers from './reducers';
+import Signin from './components/auth/signin';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
@@ -13,6 +14,7 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
+        <Route path="signin" component={Signin} />
       </Route>
     </Router>
   </Provider>
