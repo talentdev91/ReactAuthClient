1a6c6398bf383cc9be2b7baa7deb44bdf1832dd4
goldsun27 <todayhonest27@gmail.com>
2016-05-07 12:30:50 -0500

    Add Signup component

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import App from './components/app';
 import reducers from './reducers';
 import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
+import Signup from './components/auth/signup';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 
@@ -18,6 +19,7 @@ ReactDOM.render(
       <Route path="/" component={App}>
         <Route path="signin" component={Signin} />
         <Route path="signout" component={Signout} />
+        <Route path="signup" component={Signup} />
       </Route>
     </Router>
   </Provider>
