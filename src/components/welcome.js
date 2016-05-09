c386e1c8c8623bf874f6a0713d5133df8a906279
goldsun27 <todayhonest27@gmail.com>
2016-05-08 22:23:26 -0500

    Add Welcome component

---
+++ src/components/welcome.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default () => <div>Welcome Lord Commander Jon Snow</div>
--- src/index.js
+++ src/index.js
@@ -11,6 +11,8 @@ import Signin from './components/auth/signin';
 import Signout from './components/auth/signout';
 import Signup from './components/auth/signup';
 import Feature from './components/auth/feature';
+import Welcome from './components/welcome';
+import RequireAuth from './components/auth/require_auth';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
 
@@ -18,10 +20,11 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
+       <IndexRoute component={Welcome}/>
         <Route path="signin" component={Signin} />
         <Route path="signout" component={Signout} />
         <Route path="signup" component={Signup} />
-        <Route path="feature" component={Feature} />
+        <Route path="feature" component={RequireAuth(Feature)} />
       </Route>
     </Router>
   </Provider>
