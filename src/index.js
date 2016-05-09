8472b64b9ea63be6137b63cc77f45886041c00c1
goldsun27 <todayhonest27@gmail.com>
2016-05-08 22:30:45 -0500

    Update state upong login

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 import reduxThunk from 'redux-thunk';
+import {AUTH_USER} from './actions/types';
 
 import App from './components/app';
 import reducers from './reducers';
@@ -15,9 +16,18 @@ import Welcome from './components/welcome';
 import RequireAuth from './components/auth/require_auth';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+
+const token = localStorage.getItem('token');
+
+//Update state if token exists
+if (token) {
+  store.dispatch({type:AUTH_USER});
+}
+
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path="/" component={App}>
        <IndexRoute component={Welcome}/>
